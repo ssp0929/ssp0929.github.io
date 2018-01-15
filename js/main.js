87d043c9ab9225779f35f3bbaf6eb619f23c4dc7
Stephen Park <ssp0929@gmail.com>
2018-01-14 21:15:02 -0600

    Complete rework of website from ground up.

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
